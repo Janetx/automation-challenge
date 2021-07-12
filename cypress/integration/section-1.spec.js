9720fbfd98914995e43ad721283836e9f0d280f9
Janetx <1029081695@qq.com>
2021-07-12 10:07:30 -0700

    Update section-1.spec.js

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,10 @@
 const { Section1 } = require('../objects/section-1')
 
-describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+describe('Problem 1 Tables', () => {
+  it('Visits page section-1', () => {
+    cy.visit(Section1.literals.URL)
+    cy.get(Section1.elements.table).should('not.be.visible')
+    cy.get(Section1.elements.showTableButton).click()
+    cy.get(Section1.elements.tableHeader).children().its('length').should('be.eq', 5)
+    cy.get(Section1.elements.table).children().children().its('length').should('be.eq', 11)
 })
